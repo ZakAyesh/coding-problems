2a9cfd9f32b1a5f2c41bb49a7a1f7312f20cebd8
Zak Ayesh <>
2020-05-31 19:09:42 -0400

    Initial solution to problem

--- CrackingTheCodingInterview/Problem1.1/Problem1.1/Class1.cs
+++ CrackingTheCodingInterview/Problem1.1/Problem1.1/Class1.cs
@@ -2,7 +2,24 @@
 
 namespace Problem1_1
 {
-    public class Class1
+    public class UniqueCharactersInString
     {
+        public bool Solve(string text)
+        {
+            foreach (char c in text)
+            {
+                for (int i=text.IndexOf(c) + 1; i < text.Length; i++)
+                {
+                    if( c == text[1])
+                    {
+                        return false;
+
+                    }
+                        
+                }
+            }
+
+            return true;
+        }
     }
 }
---
+++ CrackingTheCodingInterview/Problem1.1/Problem1_1_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1_1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
