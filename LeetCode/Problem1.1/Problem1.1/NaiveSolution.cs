f3b46911a78da8394d60887e97670160f1bdba4c
Zak Ayesh <>
2020-06-22 23:12:05 -0400

    LeetCode Problem 1 Naive Solution Complete

---
+++ LeetCode/Problem1.1/Problem1.1/NaiveSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem1
+{
+    public class NaiveSolution: ISolution
+    {
+        public bool AreAllCharactersUnique(string text)
+        {
+            //0(1)+O(N)
+            foreach (char c in text)
+            {
+                // O(N)
+                for (int i=text.IndexOf(c) + 1; i < text.Length; i++)
+                {
+                    if( c == text[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
+//Solution: O(N^2)
---
+++ LeetCode/Problem1.1/Problem1.1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Problem1
+{
+    public class Solution: ISolution
+    {
+        public bool AreAllCharactersUnique(string text)
+        {
+            var hashSet = new HashSet<char>();
+
+            foreach (char c in text)
+            {
+                if (!hashSet.Add(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
