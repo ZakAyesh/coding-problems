8ac1cdbcda507c16da6ad5cff0d4cac0b0453b5e
Zak Ayesh <>
2020-06-23 23:59:27 -0400

    Added BigO annotations

--- LeetCode/Problem1.1/Problem1.1/Solution.cs
+++ LeetCode/Problem1.1/Problem1.1/Solution.cs
@@ -7,15 +7,16 @@ namespace Problem1
         public int[] TwoSum(int[] nums, int target)
         {
             var dict = new Dictionary<int, int>();
-
+            //O(n)
             for (int i = 0; i < nums.Length; i++)
             {
                 int complement = target - nums[i];
                 if (dict.ContainsKey(complement))
                 {
+                    //O(1)
                     return new int[] { dict[complement], i };
                 }
-
+                //O(1)
                 dict[nums[i]] = i;
             }
             return new int[] { };
